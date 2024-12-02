d95b6f9c22894398e4b87a9a96b83bfdf5dc7b1d
Sam Golden <samgolden00@gmail.com>
2024-12-02 11:24:32 -0500

    day 1 example tests and formatting

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -7,9 +7,21 @@ pub fn part_one(input: &str) -> Option<u32> {
     let mut list2: Vec<i64> = vec![];
     input.split('\n').for_each(|line| {
         let vals: Vec<&str> = line.split_ascii_whitespace().collect();
-        if vals.len() != 2 { return }
-        list1.push(vals.get(0).expect("l1 has val").parse::<i64>().expect("l1 parse val"));
-        list2.push(vals.get(1).expect("l2 has val").parse::<i64>().expect("l2 parse val"));
+        if vals.len() != 2 {
+            return;
+        }
+        list1.push(
+            vals.first()
+                .expect("l1 has val")
+                .parse::<i64>()
+                .expect("l1 parse val"),
+        );
+        list2.push(
+            vals.get(1)
+                .expect("l2 has val")
+                .parse::<i64>()
+                .expect("l2 parse val"),
+        );
     });
 
     list1.sort();
@@ -30,9 +42,23 @@ pub fn part_two(input: &str) -> Option<u32> {
     let mut freqs: HashMap<i64, i64> = HashMap::new();
     input.split('\n').for_each(|line| {
         let vals: Vec<&str> = line.split_ascii_whitespace().collect();
-        if vals.len() != 2 { return }
-        list1.push(vals.get(0).expect("l1 has val").parse::<i64>().expect("l1 parse val"));
-        *freqs.entry(vals.get(1).expect("l2 has val").parse::<i64>().expect("l2 parse val")).or_insert(0) += 1;
+        if vals.len() != 2 {
+            return;
+        }
+        list1.push(
+            vals.first()
+                .expect("l1 has val")
+                .parse::<i64>()
+                .expect("l1 parse val"),
+        );
+        *freqs
+            .entry(
+                vals.get(1)
+                    .expect("l2 has val")
+                    .parse::<i64>()
+                    .expect("l2 parse val"),
+            )
+            .or_insert(0) += 1;
     });
 
     let mut sum: u32 = 0;
@@ -50,12 +76,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(11));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
